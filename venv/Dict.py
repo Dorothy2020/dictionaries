699120e932f4976e6ba1aa59253b574cdabbe9a2
Dorothy2020 <wangarraakoth@gmail.com>
2021-05-16 09:18:31 +0300

    dictionary

---
+++ venv/Dict.py
@@ -0,0 +1,23 @@
+# Dictionary
+country_codes={"Kenya":254,"Uganda":255,"Sudan":249}
+print(country_codes)
+
+# Removing an item from Dictionary
+country_codes={"Kenya":254,"Uganda":255,"Sudan":249}
+country_codes.pop('Kenya')
+print(country_codes)
+
+# checking if a key exist in a dictionary
+
+country_codes={"Kenya":254,"Uganda":255,"Sudan":249}
+print('Kenya' in country_codes)
+print('Rwanda' in country_codes)
+
+# changing a value in a  dictionary
+country_codes={"Kenya":254,"Uganda":255,"Sudan":249}
+country_codes['Uganda']=253
+print(country_codes)
+
+
+
+
